cb1b8c73b316d8224ce469a0692bff9d75d3c39f
tech123-prog <ishika08dec.06@gmail.com>
2026-06-03 16:00:50 +0530

    Delete Assignment_1/Ishika_Gupta_02501222025/Exercise4.py

--- Assignment_1/Ishika_Gupta_02501222025/Exercise4.py
+++
@@ -1,8 +0,0 @@
-# Aim-Calculate average of 3 numbers
-a = float(input("Enter first number: "))
-b = float(input("Enter second number: "))
-c = float(input("Enter third number: "))
-
-average = (a + b + c) / 3
-
-print("Average =", average)
